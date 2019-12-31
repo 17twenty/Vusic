1b4a67a30c5a556616f6f606ef8d0a679a7d1382
falconh78 <falconhawk@protonmail.com>
2020-01-01 01:17:47 +0900

    removed api key

--- apiKeys.example.js
+++ apiKeys.example.js
@@ -1,13 +1,12 @@
 const keys = {
   appleMusic: {
-    developerToken:
-      'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldlYlBsYXlLaWQifQ.eyJpc3MiOiJBTVBXZWJQbGF5IiwiaWF0IjoxNTc2NTYwMDgzLCJleHAiOjE1OTIxMTIwODN9.s5HiUk5k48DtvkixSEd0ceWAFFSNzqVaNiORZ5O8NIVQ33Kay8H_imSBrA-jYtdt8faoREaGQ18pUSyS_fRl6Q',
+    developerToken: 'Apple MusicKitJS API Key Goes Here',
     name: 'Vusic',
     build: 'ver1',
     version: 'ver1',
   },
-  geniusAccessToken: 'Vfz06usLcwxs-t8cvv7Hcx12WPUvk9M-imk-MA3QUe0TQZAsDDGOjccEzs1Zioht',
-  youtubeApiKey: 'AIzaSyB3Ef7f5_vZvT9BiTqAYkor56x5g0FOKiQ',
+  geniusAccessToken: 'Genius Access Token Goes Here',
+  youtubeApiKey: 'Youtube Api Key Goes Here',
 };
 
 function getAppleMusicConfig() {
